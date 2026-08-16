2aabd96a9c23885c97c9ddb315976e4714ec804e
Oziel <oziel.santos2007@gmail.com>
2026-08-16 14:22:32 -0300

    finalizacoes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
